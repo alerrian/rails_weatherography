dfb87e4d8602c228149fd665691ba19c3858af0f
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-06-25 14:54:55 -0600

    Add seeds and some happy tests

--- spec/features/map_spec.rb
+++ spec/features/map_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
+  before :each do
+    @arizona = State.create!(name: 'Arizona', lat: 34.4747, long: -111.7892, zoom: 5)
+    @colorado = State.create!(name: 'Colorado', lat: 39.3026, long: -105.343061, zoom: 6)
+    @connecticut = State.create!(name: 'Connecticut', lat: 41.5000, long: -72.7000, zoom: 9)
+  end
+
   it 'can see a map on the page' do
     visit root_path
 
@@ -13,11 +19,9 @@ RSpec.describe 'As a visitor' do
     expect(page).to have_css('#controls')
   end
 
-  it 'has a drop down inside of controls that has states' do
+  it 'has a drop down' do
     visit root_path
 
-    within '#state_dropdown' do
-      
-    end
+    expect(page).to have_css('#state_dropdown')
   end
 end
