b6da07c997519d5d8f52ce5af162c2f3947fb276
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-06-25 07:32:03 -0600

    Initial Commit

---
+++ spec/models/state_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe State, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :lat }
+    it { should validate_presence_of :long }
+    it { should validate_uniqueness_of :name }
+  end
+end
+
