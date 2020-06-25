24a6403b3e7ab71772c7f081b9dfc56bccee6de4
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-06-25 10:29:28 -0600

    Add test for controls section and drop down menu

--- spec/features/map_spec.rb
+++ spec/features/map_spec.rb
@@ -2,8 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'As a visitor' do
   it 'can see a map on the page' do
-    visit '/'
+    visit root_path
 
     expect(page).to have_css('#map')
+  end
+
+  it 'has a section for controls' do
+    visit root_path
+
+    expect(page).to have_css('#controls')
   end
 end
