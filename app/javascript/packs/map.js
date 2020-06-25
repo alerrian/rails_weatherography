c37345ddcd3df784908ddcd3328f535e11073a4a
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-06-25 14:54:37 -0600

    Connect javascript to index.html.erb

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -1,4 +1,4 @@
-var mymap = L.map('map').setView([39.3026, -105.3430], 7);
+var mymap = L.map('map').setView([state_lat, state_lng], state_zoom);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
