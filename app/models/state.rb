4d9ff548b62e0d6d226f3d9a4cecb10700f681f1
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-06-26 15:16:23 -0600

    Added pins and popups for weather data related to each pin

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class State < ApplicationRecord
   validates_presence_of :name,
                         :lat,
@@ -7,6 +9,19 @@ class State < ApplicationRecord
   validates_uniqueness_of :name
 
   def get_weather(lat, long)
-    Faraday.get("http://api.openweathermap.org/data/2.5/find?lat=#{lat}&lon=#{long}&cnt=10&appid=#{ENV['OWM_API']}")
+    response = Faraday.get("http://api.openweathermap.org/data/2.5/find?lat=#{lat}&lon=#{long}&cnt=50&appid=#{ENV['OWM_API']}&units=imperial")
+    json = JSON.parse(response.body)
+
+    hash_response = hash_weather(json['list'])
+  end
+
+  def hash_weather(json_response)
+    json_response.inject({}) do |new_hash, city|
+      new_hash[city['name']] = {}
+      new_hash[city['name']][:lat] = city['coord']['lat']
+      new_hash[city['name']][:long] = city['coord']['lon']
+      new_hash[city['name']][:temp] = city['main']['temp']
+      new_hash
+    end
   end
 end
